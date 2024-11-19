50c36cb7923e5558f74319d6ba625afc05072309
W.L.A.D.D.A. Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2024-11-19 14:47:35 +0000

    Profile component added

---
+++ studentapp/src/components/Profile.js
@@ -0,0 +1,15 @@
+export default function Profile(props){
+    return(
+        <>
+        <table>
+            <tr>
+                <td rowSpan={7}>
+                    <img src={require('../assests/img/1.png')} alt="profile picture"/>
+                </td>
+                <td ID>
+                </td>
+            </tr>
+        </table>
+        </>
+    );
+}
