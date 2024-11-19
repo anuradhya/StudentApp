8a3af9fa94a9a7eb4588a1b14802e9b75e9ce52e
W.L.A.D.D.A. Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2024-11-19 14:39:31 +0000

    View button added

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -3,23 +3,25 @@ export default function StudentTable(props){
     return(
         <div className="outerDiv">
             <div className="leftDiv">
-                <table>
+                <table border="1">
                     <thead>
                         <tr>
                             <td>First Name</td>
                             <td>Last Name</td>
                             <td>Course</td>
                             <td>Country</td>
+                            <td>Profile</td>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             props.students.map(student=>
                                 <tr>
-                                    <td>{student.firstname}</td>
-                                    <td>{student.lastname}</td>
+                                    <td>{student.firstName}</td>
+                                    <td>{student.lastName}</td>
                                     <td>{student.course}</td>
                                     <td>{student.address.country}</td>
+                                    <td><button>View</button></td>
                                 </tr>
                             )
                         }
